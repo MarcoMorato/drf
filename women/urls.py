ea812521c1b623820b0c0aeb21a6251b6d51428f
Jhon <leoscorp969@yandex.ru>
2022-04-19 13:34:29 +0300

    пагинация

--- women/urls.py
+++ women/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import *
 
 from rest_framework import routers
@@ -14,6 +16,9 @@ urlpatterns = [
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/', include(router.urls)),
 
     # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
--- women/views.py
+++ women/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
@@ -13,10 +14,17 @@ from .permisisons import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
+class WomenApiListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 100000
+
+
 class WomenApiList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = WomenApiListPagination
 
 
 class WomenApiUpdate(generics.RetrieveUpdateAPIView):
